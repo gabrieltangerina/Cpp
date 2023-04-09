c12d0dd35598ca316503f4318a50814fae27e3c5
Gabriel Tangerina <110418376+gabrieltangerina@users.noreply.github.com>
2023-04-09 20:47:17 -0300

    Aprendendo sobre variáveis

---
+++ aula4.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int num;
+    char nome[20];
+    double num2;
+    float num3;
+    bool vf;
+    string texto;
+
+    cout << "Digite um número: ";
+    cin >> num;
+    cout << "Digite um nome: ";
+    cin >> nome;
+    cout << "Digite um número com decimais: ";
+    cin >> num2;
+    cout << "Digite outro número com decimais";
+    cin >> num3;
+    cout << "Digite um valor boolean: ";
+    cin >> vf;
+    cout << "Digite um texto: ";
+    cin >> texto;
+
+    return 0;
+}
