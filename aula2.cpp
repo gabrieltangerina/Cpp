f165221bc6355c1efffeea731de2bc2a6ae41626
Gabriel Tangerina <110418376+gabrieltangerina@users.noreply.github.com>
2023-04-08 21:04:32 -0300

    Aprendendo a usar cout e cin

---
+++ aula2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    char nome[20];
+
+    cout << "Digite seu nome: ";
+    cin >> nome;
+    cout << "Bem vindo, " << nome << endl;
+
+    return 0;
+}
