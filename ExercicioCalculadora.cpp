90444804ec7527b285d12076a0476f3a5dfb7680
Gabriel Tangerina <110418376+gabrieltangerina@users.noreply.github.com>
2023-04-15 11:22:30 -0300

    Exercício com do while

--- ExercicioCalculadora.cpp
+++ ExercicioCalculadora.cpp
@@ -5,42 +5,66 @@
 using namespace std;
 
 int main(){
-    setlocale(LC_ALL,"Portuguese");
+    setlocale(LC_ALL, "Portuguese");
 
     char operacao;
-    float num1, num2;
+    float num1, num2, resultado;
 
-    cout << "Digite 'S' para sair do programa" << endl;
-    cout << "Escolha a operação -> +    -    /    * : " ;
-    cin >> operacao;
-    operacao = toupper(operacao);
-    if(operacao == 'S'){
-        return;
-    }
-    cout << "Digite um número: ";
-    cin >> num1;
-    cout << "Digite outro número: ";
-    cin >> num2;
+    do{
+        cout << "Digite S para encerrar o programa!" << endl;
+        cout << "Escolha a operação: +  -  *  /: ";
+        cin >> operacao;
+        operacao = toupper(operacao);
 
+        switch(operacao){
+            case '+':
+                cout << "Soma" << endl;
+                cout << "Digite um número: ";
+                cin >> num1;
+                cout << "Digite outro número: ";
+                cin >> num2;
+                resultado = num1 + num2;
+                cout << "O resultado é: " << resultado << endl;
+                break;
+            case '-':
+                cout << "Subtração" << endl;
+                cout << "Digite um número: ";
+                cin >> num1;
+                cout << "Digite outro número: ";
+                cin >> num2;
+                resultado = num1 - num2;
+                cout << "O resultado é: " << resultado << endl;
+                break;
+            case '/':
+                cout << "Divisão" << endl;
+                cout << "Digite um número: ";
+                cin >> num1;
+                cout << "Digite outro número: ";
+                cin >> num2;
+                if(num2 == 0){
+                    cout << "ERRO na divisão" << endl;
+                }else{
+                    resultado = num1 / num2;
+                    cout << "O resultado é: " << resultado << endl;
+                }
+                break;
+            case '*':
+                cout << "Multiplicação" << endl;
+                cout << "Digite um número: ";
+                cin >> num1;
+                cout << "Digite outro número: ";
+                cin >> num2;
+                resultado = num1 * num2;
+                cout << "O resultado é: " << resultado << endl;
+                break;
+            case 'S':
+                cout << "Programa encerrado!";
+                break;
+            default:
+                cout << "Opção inválida!" << endl;
+        }
+    }while(operacao != 'S');
 
-    switch(operacao){
-        case '+':
-            cout << "O resultado é: " << num1 + num2;
-            break;
-        case '-':
-            cout << "O resultado é: " << num1 - num2;
-            break;
-        case '/':
-            cout << "O resultado é: " << num1 / num2;
-            break;
-        case '*':
-            cout << "O resultado é: " << num1 * num2;
-            break; 
-        case 's':
-            break;           
-        default:
-            cout << endl << "Opção Inválida";
-    }
 
     return 0;
 }
